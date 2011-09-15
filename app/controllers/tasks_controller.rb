2c547f358e6e269f18aeae447df2c10a9b39f816
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-09-15 22:12:05 +0200

    New task action should not expect being called nested in project.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,14 @@
 class TasksController < AuthorizedController
-  
   def new
-    @task = Task.new(:project_id => params[:project_id]) if params[:project_id]
+    # Allow callers specifying defaults
+    @task = Task.new(params[:task])
+
+    # Nested resources support
+    @task.project_id ||= params[:project_id] if params[:project_id]
+
+    # Educated guessing of defaults
     @task.person = current_user.person if current_user
     
     new!{ projects_url(@task.project) }
   end
-   
 end
