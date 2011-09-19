caa614cfe08747489008900171a07dfaae44ca67
Roman Simecek <roman.simecek@cyt.ch>
2011-09-19 15:38:54 +0200

    Added simple validations and set the calculation of the task duration right.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,14 @@ class Task < ActiveRecord::Base
   belongs_to :project
   belongs_to :person
   
+  validates :project,  :presence => true
+  validates :person,  :presence => true
+  validates :when,  :presence => true
+  validates :from,  :presence => true
+  validates :to,  :presence => true
+  
+  # The duration of the task in minutes
   def duration
-    "2h"
+    (to.to_f - from.to_f).to_i / 60
   end
 end
