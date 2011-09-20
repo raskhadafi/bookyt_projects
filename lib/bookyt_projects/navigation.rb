686cf0d5a2a8449dc2f78a1a931cb6108d619703
Roman Simecek <roman.simecek@cyt.ch>
2011-09-20 09:50:38 +0200

    Navigtion highlighting extended for the tasks.

--- lib/bookyt_projects/navigation.rb
+++ lib/bookyt_projects/navigation.rb
@@ -2,7 +2,7 @@ module BookytProjects
   module Navigation
     def setup_bookyt_projects(navigation)
       navigation.item :projects, t_model(Project), projects_path, :if => Proc.new { user_signed_in? } do |projects|
-        projects.item :project_index, t_title(:index, Project), projects_path, :highlights_on => /\/projects($|\/[0-9]*($|\/.*))/
+        projects.item :project_index, t_title(:index, Project), projects_path, :highlights_on => /\/(projects|tasks)($|\/[0-9]*($|\/.*))/
         projects.item :capture_hours, t_title(:new, Task), new_task_path
         projects.item :new_project, t_title(:new, Project), new_project_path
         projects.item :project_states, t_model(ProjectState), project_states_path, :highlights_on => /\/project_states($|\/([0-9]*|new)($|\/.*))/ 
