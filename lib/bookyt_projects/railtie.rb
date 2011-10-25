a40e7b689a8aedef3a2eae030b2b28577100365f
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-10-25 11:01:47 +0200

    Set railtie name to bookyt_projects.

--- lib/bookyt_projects/railtie.rb
+++ lib/bookyt_projects/railtie.rb
@@ -3,6 +3,8 @@ require 'rails'
 
 module BookytProjects
   class Railtie < Rails::Engine
+    engine_name "bookyt_projects"
+
     config.to_prepare do
       ::Employee.send :include, BookytProjects::Employee
     end
