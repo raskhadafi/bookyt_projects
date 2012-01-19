0d46aa19f7911bfaefbc063dca1f55d15ef51853
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-01-19 21:59:24 +0100

    Directly include BookytProjects::Person in Person.

--- lib/bookyt_projects/railtie.rb
+++ lib/bookyt_projects/railtie.rb
@@ -4,9 +4,5 @@ require 'rails'
 module BookytProjects
   class Railtie < Rails::Engine
     engine_name "bookyt_projects"
-
-    config.to_prepare do
-      ::Person.send :include, BookytProjects::Person
-    end
   end
 end
