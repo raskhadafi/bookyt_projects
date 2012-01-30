557ea27317aede1f28b9ee96bc5a3f54ef478e94
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-01-30 17:08:42 +0100

    Timesheet entry links to new activity form if none is assigned.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -13,11 +13,11 @@ class ActivitiesController < AuthorizedController
     end
 
     # Educated guessing of person
-    @activity.person = current_user.person if current_user
+    @activity.person ||= current_user.person if current_user
 
     # Educated guessing of project
     @activity.project_id ||= params[:project_id] if params[:project_id]
-    @activity.project_id ||= @activity.person.activities.order(:date).last
+    @activity.project_id ||= @activity.person.latest_project.id
 
     new!
   end
