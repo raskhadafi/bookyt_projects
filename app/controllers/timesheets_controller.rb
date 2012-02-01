61349535c8c5d3cf3204007846e0cc43c2760ffc
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2012-02-01 23:16:28 +0100

    Prepare multi timesheet views.

--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -1,5 +1,8 @@
 class TimesheetsController < ApplicationController
   def index
     @employee = Employee.find(params[:employee_id])
+    WorkDay.create_or_update_upto(@employee, Date.today.end_of_month)
+    range = Date.today.beginning_of_month..Date.today.end_of_month
+    @work_days = @employee.work_days.where(:date => range)
   end
 end
