fa7f085958b196b40c98bda2e57276b00535bd61
Andi Staub (CyT) <andi.staub@cyt.ch>
2012-03-26 11:06:47 +0200

    Updatet the navigation to bootstrap2.

--- lib/bookyt_projects/navigation.rb
+++ lib/bookyt_projects/navigation.rb
@@ -1,7 +1,7 @@
 module BookytProjects
   module Navigation
     def setup_bookyt_projects(navigation)
-      navigation.item :projects, t('bookyt.main_navigation.projects'), projects_path, :if => Proc.new { user_signed_in? } do |projects|
+      navigation.item :projects, t('bookyt.main_navigation.projects'), '#', :if => Proc.new { user_signed_in? } do |projects|
         projects.item :project_index, t_title(:index, Project), projects_path, :highlights_on => /\/(projects|activities)($|\/[0-9]*($|\/.*))/
         projects.item :new_project, t_title(:new, Project), new_project_path
         projects.item :divider, "", :class => 'divider'
