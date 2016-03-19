1e56a42e67098fc60a7adcda1726d3a26c9de82b
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2016-03-19 21:00:35 +0100

    fix(navigation): update for simpl_navigation

--- lib/bookyt_projects/navigation.rb
+++ lib/bookyt_projects/navigation.rb
@@ -4,7 +4,7 @@ module BookytProjects
       navigation.item :projects, t('bookyt.main_navigation.projects'), '#', :if => Proc.new { user_signed_in? } do |projects|
         projects.item :project_index, t_title(:index, Project), projects_path, :highlights_on => /\/(projects|activities)($|\/[0-9]*($|\/.*))/
         projects.item :new_project, t_title(:new, Project), new_project_path
-        projects.item :divider, "", :class => 'divider'
+        projects.item :divider, "", '#', :html => {:class => 'divider' }
         projects.item :activities, t_title(:index, Activity), activities_path
         projects.item :capture_hours, t('activities.new.title'), new_batch_activity_path
       end
